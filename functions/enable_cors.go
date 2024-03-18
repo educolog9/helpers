2340c5b7fd1dd73f6e561c94ede8b4c336a27eed
bujosa <davidbujosa@gmail.com>
2024-03-18 17:38:19 -0400

    feat(functions): enable cors

---
+++ functions/enable_cors.go
@@ -0,0 +1,10 @@
+package functions
+
+import "net/http"
+
+// EnableCors sets the necessary headers to enable Cross-Origin Resource Sharing (CORS) for the given http.ResponseWriter.
+func EnableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
