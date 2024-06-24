4ce072740ded12ead0f323e6d51a1e8af4257ec7
bujosa <davidbujosa@gmail.com>
2024-06-24 02:35:50 -0400

    feat(database): add learning progress

---
+++ database/mongo/learning-progress.go
@@ -0,0 +1,19 @@
+package mongo
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type LearningProgress struct {
+	ID             primitive.ObjectID `bson:"_id"`
+	Progress       float64            `bson:"progress"`
+	StudentID      primitive.ObjectID `bson:"studentId"`
+	OrganizationID primitive.ObjectID `bson:"organizationId"`
+	CourseID       primitive.ObjectID `bson:"courseId"`
+	CreatedAt      time.Time          `bson:"createdAt"`
+	Status         string             `bson:"status"`
+	Version        int                `bson:"version"`
+	UpdatedAt      time.Time          `bson:"updatedAt"`
+}
