505d90fa0216fd8d46fc97df0f9c05f3feb519b9
bujosa <davidbujosa@gmail.com>
2024-08-04 00:40:17 -0400

    feat(subscription-access): move to roles

--- enums/roles.go
+++ enums/roles.go
@@ -50,3 +50,17 @@ var ALL_ROLES = []Role{Admin, User, Author, Editor, DirectorRRHH, CoordinatorRRH
 
 // ORGANIZATION_GROUP is a list of roles that can be assigned to an organization.
 var ORGANIZATION_GROUP = []Role{Admin, DirectorRRHH, CoordinatorRRHH, User}
+
+// SubscriptionAccessType represents the access type of a subscription.
+// @name SubscriptionAccessType
+type SubscriptionAccessType string
+
+const (
+	// ManualGranted represents a subscription access manually granted by Educology
+	// @description Subscription access manually granted by Educology
+	ManualGranted SubscriptionAccessType = "manual_granted"
+
+	// SystemAuto represents a subscription access automatically granted through the payment system
+	// @description Subscription access automatically granted through the payment system
+	SystemAuto SubscriptionAccessType = "system_auto"
+)
