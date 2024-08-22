83c7d15fcc2553e9d893724be6c39b7f72ca2037
bujosa <davidbujosa@gmail.com>
2024-08-22 13:12:28 -0400

    feat: enable x-country header

--- functions/enable_cors.go
+++ functions/enable_cors.go
@@ -6,5 +6,5 @@ import "net/http"
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, x-api-key")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, x-api-key, X-Country, x-country")
 }
